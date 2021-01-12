7e12dac77e38a76f3d1be11295e365a3b09e0e4c
Charan0018 <71214315+Charan0018@users.noreply.github.com>
2021-01-12 23:08:40 +0530

    Added dice game files

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1=(Math.floor(Math.random()*6))+1;
+var imageSource1="images/dice"+randomNumber1+".png";
+document.getElementsByClassName("img")[0].setAttribute("src",imageSource1)
+var randomNumber2=(Math.floor(Math.random()*6))+1;
+var imageSource2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",imageSource2);
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="Player 1 wins👏"
+}
+else if (randomNumber1<randomNumber2) {
+  document.querySelector("h1").innerHTML="Player 2 wins👏"
+}
+else{
+  document.querySelector("h1").innerHTML="Draw"
+}
